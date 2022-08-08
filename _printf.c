6531d5593ee4db99f7c95a271b94c24b4b37cfaf
Luke Olawale <lukeolawale4@gmail.com>
2022-08-08 10:14:40 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,45 +1,66 @@
-#include "main.h"
-  2
-  3 /**
-  4  * _printf - produces output according to a format
-  5  * @format: format string containing the characters and the specifiers
-  6  * Description: this function will call the get_print() function that will
-  7  * determine which printing function to call depending on the conversion
-  8  * specifiers contained into fmt
-  9  * Return: length of the formatted output string
- 10  */
- 11 int _printf(const char *format, ...)
- 12 {
- 13         int (*pfunc)(va_list, flags_t *);
- 14         const char *p;
- 15         va_list arguments;
- 16         flags_t flags = {0, 0, 0};
- 17
- 18         register int count = 0;
- 19
- 20         va_start(arguments, format);
- 21         if (!format || (format[0] == '%' && !format[1]))
- 22                 return (-1);
- 23         if (format[0] == '%' && format[1] == ' ' && !format[2])
- 24                 return (-1);
- 25         for (p = format; *p; p++)
- 26         {
- 27                 if (*p == '%')
- 28                 {
- 29                         p++;
- 30                         if (*p == '%')
- 31                         {
- 32                                 count += _putchar('%');
- 33                                 continue;
- 34                         }
- 35                         while (get_flag(*p, &flags))
- 36                                 p++;
- 37                         pfunc = get_print(*p);
- 38                         count += (pfunc)
- 39                                 ? pfunc(arguments, &flags)
- 40                                 : _printf("%%%c", *p);
- 41                 } else
- 42                         count += _putchar(*p);
- 43         }
- 44         _putchar(-1);
+include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
